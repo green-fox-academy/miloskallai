7da110fa9f0dc1c182c74bd851ca257e6c75634f
miloskallai <milos.kallai@gmail.com>
2018-08-02 16:15:25 +0200

    TS task added

--- week-01/day-4/taskscript.ts
+++ week-01/day-4/taskscript.ts
@@ -227,14 +227,32 @@ if ( 10 < b  && b < 20) {
 // if less than 10 set out2 to "Less!",
 // if more than 20 set out2 to "More!"
 console.log(out2);
-*/
+
+// if credits are smaller than 50,
+// and is_bonus is false decrement c by 1
+// if is_bonus is true c should remain the same
+
 let c: number = 123;
 let credits: number = 100;
 let isBonus: boolean = false;
 
-// if credits are at least 50,
-// and is_bonus is false decrement c by 2
-// if credits are smaller than 50,
-// and is_bonus is false decrement c by 1
-// if is_bonus is true c should remain the same
-console.log(c);
+if (credits >= 50 && isBonus === false) {
+    console.log(c - 2);
+} else if (credits < 50 && isBonus === false){
+    console.log(c - 1);
+} else if (isBonus) {
+    console.log(c);
+}
+*/
+let d: number =  8;
+let time: number = 120;
+let out3: string = '';
+
+// if d is dividable by 4
+// and time is not more than 200
+// set out3 to "check"
+// if time is more than 200
+// set out3 to "Time out"
+// otherwise set out3 to "Run Forest Run!"
+console.log(out3);
+
