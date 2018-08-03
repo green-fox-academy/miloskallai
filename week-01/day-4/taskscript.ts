88efd245ccfbc35dd6f9f518df20e1afb701e971
miloskallai <milos.kallai@gmail.com>
2018-08-03 11:23:28 +0200

    Pyramid task finished

--- week-01/day-4/taskscript.ts
+++ week-01/day-4/taskscript.ts
@@ -335,17 +335,17 @@ for(let i: number = 1; i <= lineCount; i++){
 // The pyramid should have as many lines as lineCount is
 
 let lineCount: number = 4;
-let asterisk: string = '';
-let space: string = ' ';
+
 
 for (let i: number = 0; i <= lineCount; i++) {
-    let output: string = '';
-    for(let j: number = 0; j <= lineCount - i; j++) {
-        output += ' ';
-    } for (let k: number = 0; k <= i; k++) {
-        output +='* ';
+    let asterisk: string = '';
+    for(let j: number = 1; j <= lineCount - i; j++) {       
+         asterisk += ' ';
+    } 
+    for (let k: number = 1; k <= (2 * i + 1); k++) {
+            asterisk +='*';
     }
-    console.log(output);
+    console.log(asterisk);
 }
 
 
