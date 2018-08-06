dcfe2256ec6aded9595ad8698b5ffe23f7663d20
miloskallai <milos.kallai@gmail.com>
2018-08-06 17:22:48 +0200

    reverse first solution finished

---
+++ week-02/day-1/matrix.ts
@@ -0,0 +1,21 @@
+'use strict'
+// -  Create (dynamically*) a two dimensional list
+//    with the following matrix**. Use a loop!
+//
+//    0 0 0 1
+//    0 0 1 0
+//    0 1 0 0
+//    1 0 0 0
+//
+// -  Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+let twoDimensionaList: number [][] = [[0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0]];
+
+twoDimensionaList.forEach(function(num){
+    console.log(num);
+});
+
+console.log(twoDimensionaList[0][0]);
