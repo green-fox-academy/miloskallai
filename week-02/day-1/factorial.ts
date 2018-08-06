c7ff9c69f68ddacb1fbf0eca95548d2e21edd5eb
Milos Kallai <milos.kallai@gmail.com>
2018-08-06 21:56:43 +0200

    factorio task finished

--- week-02/day-1/factorial.ts
+++ week-02/day-1/factorial.ts
@@ -1,16 +1,16 @@
 // -  Create a function called `factorio`
 //    that returns it's input's factorial
-export{}
+export { };
 
-function factorio(inputNumber: number): number{
-for(let i: number = 1; i < inputNumber + 1; i++){ 
-    let numTwo: number = 2;
-    numTwo *= i;
-    return numTwo; 
-    }
+function factorio(inputNumber: number): number {
+    if (inputNumber === 0  || inputNumber === 1) 
+        return 1;
+        for(let i: number = inputNumber - 1; i >= 1; i--){ 
+           // console.log(i);
+        inputNumber = inputNumber * i;
+        }
+        return inputNumber;
 }
 
-console.log(factorio(4));
-
-//3! = 3 * 2 * 1 = 6
-//4! = 4 * 3 * 2 * 1 = 24
+let result = factorio(4);
+console.log(result);
--- week-02/day-1/reverse-list.ts
+++ week-02/day-1/reverse-list.ts
@@ -7,10 +7,23 @@
 //	   -  do it with creating a new temp array and a loop
 // -  Print the elements of the reversed `numList`
 
+
+//First solution 
+/*
 let numList: number [] = [3, 4, 5, 6, 7];
 
 let firstSolution: any = numList.reverse(); 
 
 console.log(firstSolution);
+*/
 
-let secondSolution = 
+//Second solution
+let numList: number [] = [3, 4, 5, 6, 7];
+
+for(let i: number = 0; i < numList.length; i++){
+    // push the last element of the array to a new one
+    
+}
+   
+
+
