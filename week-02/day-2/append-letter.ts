af0f074cbe8b578f1baed8c496fcb0b5601a2b86
miloskallai <milos.kallai@gmail.com>
2018-08-07 16:42:46 +0200

    append a task finished

--- week-02/day-2/append-letter.ts
+++ week-02/day-2/append-letter.ts
@@ -3,6 +3,11 @@
 // The parameter should be a list.
 let far: string[] = ['bo', 'anacond', 'koal', 'pand', 'zebr'];
 
+function appendA (array: string[]): any {
+    let result = array.map(x => x + 'a');
+    return result;
+}
+
 console.log(appendA(far));
 
 // The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
