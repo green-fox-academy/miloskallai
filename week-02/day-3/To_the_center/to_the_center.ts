382587a0689500733be9588117f227efb43d80aa
miloskallai <milos.kallai@gmail.com>
2018-08-08 12:57:10 +0200

    to the center task finished

---
+++ week-02/day-3/To_the_center/to_the_center.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a line drawing function that takes 2 parameters:
+
+function lineDrawing(x: number, y:number){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+    ctx.lineTo(canvas.width / 2, canvas.height/2);
+    ctx.stroke();
+}
+
+//lineDrawing(0, 0);
+
+// The x and y coordinates of the line's starting point
+// and draws a line from that point to the center of the canvas
+// Draw 3 lines with that function. Use loop for that.
+
+for(let i: number = 0; i < 3; i++) {
+    let randomX: number = Math.floor(Math.random() * (canvas.width));
+    let randomY = Math.floor(Math.random() * (canvas.height));
+        lineDrawing(randomX, randomY);
+}
