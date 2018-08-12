e9cde6d68c0ab602c2d5b90fea8189876769ca80
Milos Kallai <milos.kallai@gmail.com>
2018-08-12 19:51:25 +0200

    he will never task finished

---
+++ week-02/day-2/he_will_never.ts
@@ -0,0 +1,30 @@
+'use strict';
+// Things are a little bit messed up
+// Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+// Assemble the fragments into the out variable
+let out = '';
+let notSoCrypticMessage = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
+
+let hashmap = {
+  7: 'run around and desert you',
+  50: 'tell a lie and hurt you ',
+  49: 'make you cry, ',
+  2: 'let you down',
+  12: 'give you up, ',
+  1: 'Never gonna ',
+  11: '\n',
+  3: 'say goodbye '
+};
+
+let messageWithoutDuplicates = notSoCrypticMessage.filter(function(item, index){
+  return notSoCrypticMessage.indexOf(item) >= index;
+});
+
+let temp = [];
+for (const key of Object.keys(hashmap)) {
+  temp.push(hashmap[key])
+}
+
+out = temp.toString()
+
+console.log(out)
