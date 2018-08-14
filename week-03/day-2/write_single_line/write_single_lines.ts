c389d7b6682d583257e1c8ebdf2d0c269ee6a9a2
miloskallai <milos.kallai@gmail.com>
2018-08-14 15:05:24 +0200

    write single line task finished

--- week-03/day-2/write_single_line/write_single_lines.ts
+++ week-03/day-2/write_single_line/write_single_lines.ts
@@ -2,11 +2,22 @@
 // Write your name in it as a single line
 // If the program is unable to write the file,
 // then it should print an error message like: 'Unable to write file: my-file.txt'
-'use strict'
+"use strict";
 
 declare function require(path: string): any;
 
-const fs = require('fs');
+const fs = require("fs");
 
-let fileContent = "Milos Kallai";
-fs.writeFileSync('my-file.txt', fileContent);
+function writeMyName(fileName: string, name: string) {
+  try {
+    if (fs.existsSync(fileName)) {
+      fs.writeFileSync(fileName, name);
+    } else {
+      throw new Error("Unable to write file: my-file.txt");
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+writeMyName("my-fsdfsdfdssdffile.txt", "Milos Kallai");
