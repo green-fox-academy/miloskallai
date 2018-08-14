5b3f74fb465a6ada99e7a6846708ec1cf27f3303
miloskallai <milos.kallai@gmail.com>
2018-08-14 16:52:09 +0200

    GET/POST ratio function working

---
+++ week-03/day-2/logs/logs.ts
@@ -0,0 +1,30 @@
+"use strict";
+
+// Read all data from 'log.txt'.
+// Each line represents a log message from a web server
+// Write a function that returns an array with the unique IP adresses.
+// Write a function that returns the GET / POST request ratio.
+
+declare function require(sourceFile: string): any;
+
+const fs = require("fs");
+
+let fileContent = fs.readFileSync("log.txt", "utf-8");
+
+function getPostRatio(file): number {
+  let result = file.split(" ");
+
+  let post = 0;
+  let get = 0;
+
+  for (let i: number = 0; i < result.length; i++) {
+    if (result[i] === "POST") {
+      post += 1;
+    } else if (result[i] === "GET") {
+      get += 1;
+    }
+  }
+  return get / post;
+}
+
+console.log(getPostRatio(fileContent));
