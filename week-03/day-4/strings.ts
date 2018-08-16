07ca5fe115857b07e37e498b7f979bd95711b7b5
miloskallai <milos.kallai@gmail.com>
2018-08-16 16:57:03 +0200

    strings task finished

--- week-03/day-4/strings.ts
+++ week-03/day-4/strings.ts
@@ -1,4 +1,14 @@
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
 
-let exampleString: string = 'erxxtxzxuhijxxsfsxxslxfsfsx';
+let xString: string = "saXgxgsfggyhygxhgyXgyxxxghXdxhgsdgxhpXxm";
+
+function xToY(toChange: string): string {
+  if (toChange.indexOf("x") !== -1) {
+    toChange = toChange.replace("x", "y");
+    return xToY(toChange);
+  }
+  return toChange;
+}
+
+console.log(xToY(xString));
