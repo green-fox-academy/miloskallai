011ca2a37a9c5d98917451368e538be73513d016
miloskallai <milos.kallai@gmail.com>
2018-08-18 17:11:20 +0200

    sort that list finished

---
+++ week-02/day-1/sort-that-list.ts
@@ -0,0 +1,34 @@
+//  Create a function that takes a list of numbers as parameter
+//  Returns a list where the elements are sorted in ascending numerical order
+//  Make a second boolean parameter, if it's `true` sort that list descending
+
+function bubble(numbersToSort: number[]): number[] {
+  let result: number[] = numbersToSort.sort(function(a, b) {
+    return a - b;
+  });
+  return result;
+}
+
+function advancedBubble(
+  numbersToSort: number[],
+  isDescending: boolean
+): number[] {
+  let result: number[] = [];
+
+  if (isDescending) {
+    result = numbersToSort.sort(function(a, b) {
+      return b - a;
+    });
+  } else {
+    result = numbersToSort.sort(function(a, b) {
+      return a - b;
+    });
+  }
+  return result;
+}
+
+//  Example:
+console.log(bubble([34, 12, 24, 9, 5]));
+//  should print [5, 9, 12, 24, 34]
+console.log(advancedBubble([34, 12, 24, 9, 5], true));
+//  should print [34, 24, 12, 9, 5]
