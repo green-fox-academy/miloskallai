8d205ec1fc9a38c3dba17211c510ecdd50b7c891
miloskallai <milos.kallai@gmail.com>
2018-08-20 11:50:11 +0200

    test completed

--- week-04/day-2/before.ts
+++ week-04/day-2/before.ts
@@ -19,9 +19,6 @@ class Person {
 
 }
 
-let janeDoe = new Person('Jane Doe', 30, 'female');
-
-
 class Student {
 
   name: string;
@@ -52,9 +49,6 @@ class Student {
 
 }
 
-let janeDoeStudent = new Student('Jane Doe', 30, 'female', 'The School of Life');
-
-
 class Mentor {
   name: string;
   age: number;
@@ -78,9 +72,6 @@ class Mentor {
 
 }
 
-let janeDoeMentor = new Mentor('Jane Doe', 30, 'female', 'intermediate');
-
-
 class Sponsor {
   name: string;
   age: number;
@@ -97,17 +88,49 @@ class Sponsor {
   }
 
   hire(studentsToHire: number){
-    this.hiredStudents += studentsToHire;
+    this.hiredStudents += 1;
   }
 
-  constructor(name: string, age: number, gender: string, company){
+  constructor(name: string, age: number, gender: string, company:string){
     this.name = name;
     this.age = age;
     this.gender = gender;
     this.hiredStudents = 0;
+    this.company = company;
   }
 
 }
 
-let janeDoeSponsor = new Mentor('Jane Doe', 30, 'female', 'Google');
+//TEST INPUT
+let people = [];
+let mark = new Person('Mark', 46, 'male');
+people.push(mark);
+let jane = new Person('Jane Doe', 30, 'female');
+people.push(jane);
+let john = new Student('John Doe', 20, 'male', 'BME');
+people.push(john);
+let student = new Student('Jane Doe', 30, 'female', 'The School of Life');
+people.push(student);
+let gandhi = new Mentor('Gandhi', 148, 'male', 'senior');
+people.push(gandhi);
+let mentor = new Mentor('Jane Doe', 30, 'female', 'intermediate');
+people.push(mentor);
+let sponsor = new Sponsor('Jane Doe', 30, 'female', 'Google');
+people.push(sponsor);
+let elon = new Sponsor('Elon Musk', 46, 'male', 'SpaceX');
+people.push(elon)
 
+student.skipDays(3);
+
+for (let i = 0; i < 3; i++) {
+  elon.hire(i);
+}
+
+for (let i = 0; i < 5; i++) {
+  sponsor.hire(i);
+}
+
+for (let person of people) {
+  person.introduce();
+  person.getGoal();
+}
