d436ddb28fc88474cd5b0a010cd1c4b537b62afe
miloskallai <milos.kallai@gmail.com>
2018-09-12 12:24:57 +0200

    image src and a href changed

---
+++ week-06/day-3/exercise_3/script.js
@@ -0,0 +1,7 @@
+let imgURL = document.querySelector('img');
+let linkToGF = document.querySelector('a');
+
+console.log(imgURL);
+
+imgURL.setAttribute('src', 'https://i.imgflip.com/issmr.jpg');
+linkToGF.setAttribute('href', 'https://www.greenfoxacademy.com/');
