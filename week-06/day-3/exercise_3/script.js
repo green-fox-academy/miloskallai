60026cc71aae656d12b5adce50dd80be357a9631
miloskallai <milos.kallai@gmail.com>
2018-09-12 12:27:48 +0200

    second button disabeled

--- week-06/day-3/exercise_3/script.js
+++ week-06/day-3/exercise_3/script.js
@@ -1,7 +1,10 @@
 let imgURL = document.querySelector('img');
 let linkToGF = document.querySelector('a');
+let buttonToDisable = document.querySelector('.this-one');
 
 console.log(imgURL);
 
 imgURL.setAttribute('src', 'https://i.imgflip.com/issmr.jpg');
 linkToGF.setAttribute('href', 'https://www.greenfoxacademy.com/');
+
+buttonToDisable.disabled = true;
