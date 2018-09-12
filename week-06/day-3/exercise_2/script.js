acac55f2f19e20071ddd7e0465aada6e369eae44
miloskallai <milos.kallai@gmail.com>
2018-09-12 12:14:16 +0200

    exercise 1.2. completed

---
+++ week-06/day-3/exercise_2/script.js
@@ -0,0 +1,9 @@
+let list = document.querySelectorAll('li');
+let unorderedList = document.querySelector('ul');
+
+list[0].textContent = 'apple';
+list[1].textContent = 'banana';
+list[2].textContent = 'cat';
+list[3].textContent = 'dog';
+
+unorderedList.style.backgroundColor = 'limegreen';
