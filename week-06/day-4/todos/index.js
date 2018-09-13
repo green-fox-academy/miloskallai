ece6d8cc95672dbbfd0858a7243ce83104f5746b
miloskallai <milos.kallai@gmail.com>
2018-09-13 15:40:15 +0200

    todo task finished

--- week-06/day-4/todos/index.js
+++ week-06/day-4/todos/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const todos = ['get up', 'survive', 'go back to bed',];
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    todo1: todos[0],
+    todo2: todos[1],
+    todo3: todos[2],
+  });
 });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
