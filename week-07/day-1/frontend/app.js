26c97dc2990e55f23929840b8cd7b466be68b50b
miloskallai <milos.kallai@gmail.com>
2018-09-17 12:18:54 +0200

    doubling task completed

--- week-07/day-1/frontend/app.js
+++ week-07/day-1/frontend/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-PORT = 3000;
+PORT = 8080;
+
+app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/doubling', (req, res) => {
+  let received = req.query.input;
+  if (received) {
+    res.json({
+      received: received,
+      result: received * 2
+    });
+  } else {
+    res.json({
+      error: 'Please provide an input!'
+    });
+  }
 });
 
 app.listen(PORT, () => {
