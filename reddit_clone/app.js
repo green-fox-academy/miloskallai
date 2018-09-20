26ff9c16b8934989b29e42f498aec26078200734
miloskallai <milos.kallai@gmail.com>
2018-09-20 11:25:27 +0200

    (feat) database connected to app.js

--- reddit_clone/app.js
+++ reddit_clone/app.js
@@ -3,7 +3,34 @@ const app = express();
 const mysql = require('mysql');
 const PORT = 8080;
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'hello',
+  database: 'reddit'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('Connected to database');
+});
+
+let query = 'SELECT * FROM posts';
+
+connection.query(query, (err, result) => {
+  if (err) throw err;
+
+  console.log('Data received from database');
+  console.log(result);
+});
+
+connection.end();
+
 app.get('/hello', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/posts', (req, res) => {
   res.send('Hello world');
 });
 
