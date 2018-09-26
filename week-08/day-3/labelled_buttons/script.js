91e00243f910fb257f837039a107667fe04f65b2
miloskallai <milos.kallai@gmail.com>
2018-09-26 14:04:46 +0200

    label works with js

---
+++ week-08/day-3/labelled_buttons/script.js
@@ -0,0 +1,10 @@
+let text = document.querySelector('.text');
+let icon = document.querySelector('.icon');
+
+icon.addEventListener('mouseenter', ()=>{
+  text.style.visibility = 'visible';
+})
+
+icon.addEventListener('mouseleave', ()=>{
+  text.style.visibility = 'hidden';
+})
