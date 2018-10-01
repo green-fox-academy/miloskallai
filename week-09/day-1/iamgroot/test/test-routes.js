9d6654c80e1563fdd40e615054108339e0518189
miloskallai <milos.kallai@gmail.com>
2018-10-01 16:59:59 +0200

    test done

--- week-09/day-1/iamgroot/test/test-routes.js
+++ week-09/day-1/iamgroot/test/test-routes.js
@@ -1,9 +1,22 @@
 'use strict';
-​
+
 const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
-​
+
 test('groot endpoint', (t) => {
   // TODO: implement it 
+  request(app)
+  .get('/groot')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .end((err, res) => {
+    if (err) {
+      t.error(err);
+    } 
+    t.deepEqual(res.body, {error: 'I am Groot!'});
+  });
+  t.end();
 });
+
+
