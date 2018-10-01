3f91e13fdbb4f2e4b844fda982505a59130ec9aa
miloskallai <milos.kallai@gmail.com>
2018-10-01 17:28:33 +0200

    (feat)email and name can displayed on index.html

--- week-08/day-4/newsletter/index.js
+++ week-08/day-4/newsletter/index.js
@@ -1,14 +1,37 @@
-const url = 'http://localhost:3000/singup';
+window.onload = () => {
+  const url = 'http://localhost:3000/singup';
 
-let btn = document.querySelector('button');
+  let btn = document.querySelector('button');
+  const form = document.querySelector('form');
+  const resultContainer = document.querySelector('.result-container');
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+  
+    const { name, email } = event.target.elements;
+    let body = { name: name.value, email: email.value };
+  
+    fetch(url, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(body)
+    }).then(response => {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(response => {
+          const nameDisplay = document.createElement('p');
+          nameDisplay.innerHTML = response.name;
+          resultContainer.appendChild(nameDisplay);
 
-btn.addEventListener('click', () => {
-  fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
+          const emailDisplay = document.createElement('p');
+          emailDisplay.innerHTML = response.email;
+          resultContainer.appendChild(emailDisplay);
+
+        })
+      }
     });
-  //event.preventDefault();
-});
+  });
+}
+
--- week-08/day-4/newsletter/server.js
+++ week-08/day-4/newsletter/server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
+
 app.post('/singup', (req, res)=>{
   console.log(req.body);
   res.json(req.body);
