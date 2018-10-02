57aabb92442be82f6528298a05fd762b4e5b740e
miloskallai <milos.kallai@gmail.com>
2018-10-02 13:24:12 +0200

    uneccesary logs removed

--- week-09/day-2/promise_it_wont_hurt/index.js
+++ week-09/day-2/promise_it_wont_hurt/index.js
@@ -1,25 +1,26 @@
-'use strict'
+'use strict';
 
 const url = 'http://api.icndb.com/jokes/random';
 
-
 window.onload = () => {
-const btn = document.querySelector('.btn-joke');
-const jokeContainer = document.querySelector('.joke-container');
-btn.addEventListener('click', () => {
-
-fetch(url)
-.then(res => {
-  return res.json() 
-})
-.then(myJson => {
-  console.log(myJson.value.joke);
-  let joke = document.createElement('p');
-  joke.innerHTML = myJson.value.joke;
-  jokeContainer.appendChild(joke);
-
-})
-
-
-})
-}
+  const btn = document.querySelector('.btn-joke');
+  const jokeContainer = document.querySelector('.joke-container');
+  btn.addEventListener('click', () => {
+    fetch(url)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Something went wrong');
+        }
+      })
+      .then(myJson => {
+        let joke = document.createElement('p');
+        joke.innerHTML = myJson.value.joke;
+        jokeContainer.appendChild(joke);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+};
