0eea644a18620ae55ff264d5faf512645ee31fdd
miloskallai <milos.kallai@gmail.com>
2018-10-02 13:00:21 +0200

    (feat) api fetch created in index js

--- week-09/day-2/promise_it_wont_hurt/index.js
+++ week-09/day-2/promise_it_wont_hurt/index.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const url = 'http://api.icndb.com/jokes/random';
+const btn = document.querySelector('.btn-joke');
+
+window.onload = () => {
+btn.addEventListener('click', () => {
+
+fetch(url)
+.then(res => {
+  return res.json() 
+})
+.then(myJson => {
+  console.log(myJson);
+})
+
+
+})
+}
