67b78f0131611014025a5d30b71ae52e53ef8054
miloskallai <milos.kallai@gmail.com>
2018-10-04 21:33:00 +0200

    (feat) new questions can be added to database

--- week-09/day-4/quiz_application/app.js
+++ week-09/day-4/quiz_application/app.js
@@ -78,42 +78,116 @@ app.get('/questions', (req, res) => {
 
 app.post('/questions', (req, res) => {
   let newQuestion = req.body.question;
-  let realQuestionId;
-
+  let questionID;
   let answer1 = req.body.answer1;
   let answer2 = req.body.answer2;
   let answer3 = req.body.answer3;
   let answer4 = req.body.answer4;
-
   let isCorrect1 = req.body.is_correct1;
   let isCorrect2 = req.body.is_correct2;
   let isCorrect3 = req.body.is_correct3;
   let isCorrect4 = req.body.is_correct4;
 
-  if(isCorrect1 === undefined){
-    isCorrect1 = 0
+  if (isCorrect1 === undefined) {
+    isCorrect1 = 0;
   } else {
     isCorrect1 = 1;
   }
 
-  if(isCorrect2 === undefined){
-    isCorrect2 = 0
+  if (isCorrect2 === undefined) {
+    isCorrect2 = 0;
   } else {
     isCorrect2 = 1;
   }
 
-  if(isCorrect3 === undefined){
-    isCorrect3 = 0
+  if (isCorrect3 === undefined) {
+    isCorrect3 = 0;
   } else {
     isCorrect3 = 1;
   }
 
-  if(isCorrect4 === undefined){
-    isCorrect4 = 0
+  if (isCorrect4 === undefined) {
+    isCorrect4 = 0;
   } else {
     isCorrect4 = 1;
   }
 
+  let values = {
+    question: newQuestion
+  };
+
+  connection.query('INSERT INTO questions SET ? ', values, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      let questionID = result.insertId;
+      console.log(result);
+
+      let answerValues1 = {
+        question_id: questionID,
+        answer: answer1,
+        is_correct: isCorrect1
+      };
+      connection.query(
+        'INSERT INTO answers SET ? ',
+        answerValues1,
+        (err, result) => {
+          if (err) {
+            throw err;
+          } else {
+            let answerValues2 = {
+              question_id: questionID,
+              answer: answer2,
+              is_correct: isCorrect2
+            };
+
+            connection.query(
+              'INSERT INTO answers SET ? ',
+              answerValues2,
+              (err, result) => {
+                if (err) {
+                  throw err;
+                } else {
+                  let answerValues3 = {
+                    question_id: questionID,
+                    answer: answer3,
+                    is_correct: isCorrect3
+                  };
+                  connection.query(
+                    'INSERT INTO answers SET ? ',
+                    answerValues3,
+                    (err, result) => {
+                      if (err) {
+                        throw err;
+                      } else {
+                        let answerValues4 = {
+                          question_id: questionID,
+                          answer: answer4,
+                          is_correct: isCorrect4
+                        };
+                        connection.query(
+                          'INSERT INTO answers SET ? ',
+                          answerValues4,
+                          (err, result) => {
+                            if (err) {
+                              throw err;
+                            } else {
+                              console.log('Database updated');
+                              res.redirect('/');
+                            }
+                          }
+                        );
+                      }
+                    }
+                  );
+                }
+              }
+            );
+          }
+        }
+      );
+    }
+  });
 });
 
 app.delete('/questions/:id', (req, res) => {
